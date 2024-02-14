39b29ddd039f3ee96248f39015028e5ff23df975
Sahanaroy1 <sahanaroy20@gmail.com>
2024-02-14 12:31:50 +0000

    build the basic structure of the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "Javascript is an _______ language?",
+        choices: [ 'Object-oriented','Object-Based', 'Procedural', 'None of the above'],
+        answer: 'Object-oriented',
+    },
+    {
+        title: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: [ 
+            'getElementbyId()',
+            'getElementbyClassName()', 
+            'Both the above',
+            'None of the above'],
+        answer: 'Both the above',
+    },
+    {
+        title: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: [ 'var','let', 'var and let', 'None of the above'],
+        answer: 'var and let',
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: [ 'strings','booleans', 'alerts', 'numbers'],
+        answer: 'alerts',
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within---.",
+        choices: [ 'quotes','curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses',
+    },
+]
---
+++ assets/js/script.js
@@ -0,0 +1,117 @@
+var startQuizEl = document.getElementById("startQuiz")
+var welcomeEl = document.getElementById("welcome")
+var quizEl = document.getElementById("quiz")
+var resultEl = document.getElementById("results")
+var optionEl = document.getElementById("options")
+var messageEl = document.getElementById("message")
+var timerEl = document.getElementById("timer")
+var summaryEl = document.getElementById("summary")
+var playAgainEL = document.getElementById("playAgain");
+
+var secondsLeft = 0;
+var score = 0;
+var currentQuestion = 0;
+
+var countdownTimer;
+
+function stopGame(){
+    clearInterval(countdownTimer);
+
+    timerEl.textContent = ""
+
+   
+    resultEl.style.display = 'flex';
+    summaryEl.textContent = "Your Score is " + score;
+}
+
+function onSelectAnswer(e){
+    var correctAnswer = questions[currentQuestion].answer;
+    var userAnswer = e.target.textContent;
+    
+    if(correctAnswer === userAnswer){
+        score++;
+        showMessage( "Yipee :)");
+    }else{
+        secondsLeft -= 10;
+        showMessage("Oops Wrong :(");
+    }
+    
+    
+    currentQuestion++;
+    console.log(currentQuestion);
+    if(currentQuestion >= questions.length){
+        console.log("In stop");
+        stopGame();
+    }else{
+        console.log("out");
+        displayQuestion();
+    }
+    
+}
+function showMessage(msg){
+    document.getElementById("message").textContent = msg;
+     setInterval(function(){
+        document.getElementById("message").textContent = '';
+    }, 3000);
+    
+}
+
+function displayQuestion(){
+
+    startQuizEl.style.display = 'none';
+    console.log("Current question is " + currentQuestion);
+
+
+    if(currentQuestion >= questions.length){
+        stopGame();
+        return;
+    }
+    var question = questions[currentQuestion];
+    document.getElementById("question").textContent = question.title;
+
+    displayAnswer();
+    
+}
+
+
+function displayAnswer(){
+    document.getElementById('options').innerHTML = '';
+    const ul = document.getElementById('options');
+    var answer = questions[currentQuestion];
+  
+    for (i = 0; i < 4; i++) {//change to answer choices
+      const li = document.createElement("li"); 
+  
+      li.innerHTML = answer.choices[i]; 
+
+      
+  
+      ul.appendChild(li); 
+    }
+}
+
+function onStartGame(){
+    resultEl.style.display = "none";
+    displayQuestion();
+    secondsLeft = 75;
+
+    score = 0;
+
+    countdownTimer = setInterval(function(){
+        if(secondsLeft > 0){
+            timerEl.textContent = secondsLeft;
+        }else{
+            stopGame();
+        }
+        secondsLeft--;
+    }, 1000);
+   
+}
+
+
+
+
+startQuizEl.addEventListener("click", onStartGame);
+optionEl.addEventListener("click", onSelectAnswer);
+playAgainEL.addEventListener("click", onStartGame);
+
