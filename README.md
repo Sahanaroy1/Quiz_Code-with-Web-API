bc7e76cb06e03eaeacffe5500e2c2a5d36a38921
Sahanaroy1 <sahanaroy20@gmail.com>
2024-02-15 13:55:38 +0000

    Added readme and added new function for result div

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Quiz_Code-with-Web-API
+
+## User Story
+
+* As a web developer this week's challenge is to build a timed based quiz with multiple-choice questions. This code should potray dynamic HTML CSS Javescript code with resposive user interface.
+
+* To provide a dynamic website the score should be saved so that it could be compared with co-players.
+
+## Acceptance Criteria
+* It's done when the Start Quiz button is clicked a question appear with a set of answers.
+* It's done when the first question appears the timer also starts.
+* It's done when the question is answered the next question comes up. If any wrong answer is given 10 secs is deducted from the timer.
+* It's done when the all questions are answersed the timer stops and the game is over.
+* It's done when the game is over the score can be saved with my initials.
+
+
+* Deployed Link:
+
+
+# Snippet of the Quiz:
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 var startQuizEl = document.getElementById("startQuiz")
-var welcomeEl = document.getElementById("welcome")
 var quizEl = document.getElementById("quiz")
 var resultEl = document.getElementById("results")
 var optionEl = document.getElementById("options")
-var messageEl = document.getElementById("message")
 var timerEl = document.getElementById("timer")
 var summaryEl = document.getElementById("summary")
 var playAgainEL = document.getElementById("playAgain");
+var saveScoreEl = document.getElementById("saveScore");
+var initialEl = document.getElementById("initial")
+var viewScoreEl = document.getElementById("viewScore")
+var hrefHighScoreEl = document.getElementById("hrefHighScore")
 
 var secondsLeft = 0;
 var score = 0;
@@ -14,6 +16,7 @@ var currentQuestion = 0;
 
 var countdownTimer;
 
+//this function remove the timer and result screen comes up
 function stopGame(){
     clearInterval(countdownTimer);
 
@@ -23,88 +26,17 @@ function stopGame(){
    
     resultEl.style.display = 'flex';
 
-   
-    var initial = prompt("Enter your name or initials:");
-    
-  
-    localStorage.setItem("initial", initial);
-    localStorage.setItem("score", score);
+    summaryEl.textContent = "You scored " + score + ".";
 
-    summaryEl.textContent = "Hey!" + initial + " you scored " + score + ".";
+    document.getElementById("timer").style.display = "none";
 }
-
-
-
-function onSelectAnswer(e){
-    var correctAnswer = questions[currentQuestion].answer;
-    var userAnswer = e.target.textContent;
-    
-    if(correctAnswer === userAnswer){
-        score++;
-        showMessage( "Yipee :)");
-    }else{
-        secondsLeft -= 10;
-        showMessage("Oops Wrong :(");
-    }
-    
-    
-    currentQuestion++;
-    console.log(currentQuestion);
-    if(currentQuestion >= questions.length){
-        stopGame();
-    }else{
-        displayQuestion();
-    }
-    
-}
-function showMessage(msg){
-    document.getElementById("message").textContent = msg;
-     setInterval(function(){
-        document.getElementById("message").textContent = '';
-    }, 3000);
-    
-}
-
-function displayQuestion(){
-
-    startQuizEl.style.display = 'none';
-    console.log("Current question is " + currentQuestion);
-
-
-    if(currentQuestion >= questions.length){
-        stopGame();
-        return;
-    }
-    var question = questions[currentQuestion];
-    document.getElementById("question").textContent = question.title;
-
-    displayAnswer();
-    
-}
-
-
-function displayAnswer(){
-    document.getElementById('options').innerHTML = '';
-    const ul = document.getElementById('options');
-    var answer = questions[currentQuestion];
-  
-    for (i = 0; i < 4; i++) {//change to answer choices
-      const li = document.createElement("li"); 
-  
-      li.innerHTML = answer.choices[i]; 
-
-      
-  
-      ul.appendChild(li); 
-    }
-}
-
+//Function created to start game and the timer starts
+//the displayQuestion function from this function
 function onStartGame(){
     resultEl.style.display = "none";
     document.getElementById("welcomeNote").style.display = "none";
     displayQuestion();
     secondsLeft = 75;
-
     score = 0;
 
     countdownTimer = setInterval(function(){
@@ -117,12 +49,125 @@ function onStartGame(){
     }, 1000);
    
 }
+//Is created to display question and the display function is called from here
+function displayQuestion(){
+    startQuizEl.style.display = 'none';
+    console.log("Current question is " + currentQuestion);
+    if(currentQuestion >= questions.length){
+        stopGame();
+        return;
+    }
+    var question = questions[currentQuestion];
+    document.getElementById("question").textContent = question.title;
+    displayAnswer();
+}
 
+//function is made to display the answers options
+function displayAnswer(){
+    document.getElementById('options').innerHTML = '';
+    const ul = document.getElementById('options');
+    var answer = questions[currentQuestion];
+  
+    for (i = 0; i < 4; i++) {//change to answer choices
+      const li = document.createElement("li"); 
+      li.innerHTML = answer.choices[i]; 
+      ul.appendChild(li); 
+    }
+}
 
+//created the function to match the correct answer to user's answer
+//showMessage function is also called from here
+function onSelectAnswer(e){
+    var correctAnswer = questions[currentQuestion].answer;
+    var userAnswer = e.target.textContent;
+    
+    if(correctAnswer === userAnswer){
+        score++;
+        showMessage( "Yipee :)");
+    }else{
+        secondsLeft -= 10;
+        showMessage("Oops Wrong :(");
+    }
+    currentQuestion++;
+    console.log(currentQuestion);
+    if(currentQuestion >= questions.length){
+        stopGame();
+    }else{
+        displayQuestion();
+    }
+    
+//this function is made to set the timer for the message which will disaapear after 3 secs    
+}
+function showMessage(msg){
+    document.getElementById("message").textContent = msg;
+     setInterval(function(){
+        document.getElementById("message").textContent = '';
+    }, 3000);  
+}
 
+//On save score function is created to save the score in the local storage
+function onSaveScore (e) {
 
+    var initial = initialEl.value;
+    if (initial === "") {
+        alert("Please enter your initial");
+        return;
+    }
+    localStorage.setItem(initial, score);
+    document.getElementById("initial").value = "";   
+}
+
+//this function helps to display the score on the screen
+function displayScores(){
+    //const scoresListEl = document.getElementById("scores-list")
+    resultEl.style.display = "none";
+    console.log("display score")
+
+    const ul = document.getElementById("scores-list");
+    console.log(localStorage.getItem("A"));
+    for (var i = 0; i < 10; i++){
+        const li = document.createElement("li"); 
+        var initial = localStorage.key(i);
+        var score = localStorage.getItem(localStorage.key(i));
+        li.innerHTML = initial + " - " + score;
+     ul.appendChild(li);
+    }
+    document.getElementById("highScores").style.display = "flex";
+    document.getElementById("scores-list").style.display = "flex";  
+}
+
+//this is called from the play again button to restart the game
+function playAgain(){
+    currentQuestion = 0;
+    
+    onStartGame();
+    quizEl.style.display = 'flex';
+    quizEl.style.flexDirection = "column"
+    
+}
+
+//this function helps to show the score when the a tag of high score is clicked
+function highScoreHref(){
+    displayScores();
+
+    document.getElementById("highScores").style.display = "flex";
+    document.getElementById("highScores").style.marginTop = "-100px";
+    document.getElementById("scores-list").style.display = "flex";
+    document.getElementById("welcomeNote").style.display = "none";
+    document.getElementById("startQuiz").style.display = "none";
+    document.getElementById("reset").style.display = "flex";
+
+    
+}
+ 
+//the eventlisteners are helps the buttons to do their respective function
 startQuizEl.addEventListener("click", onStartGame);
 optionEl.addEventListener("click", onSelectAnswer);
-playAgainEL.addEventListener("click", onStartGame);
+playAgainEL.addEventListener("click", playAgain);
+saveScoreEl.addEventListener("click", onSaveScore);
+viewScoreEl.addEventListener("click", displayScores);
+hrefHighScoreEl.addEventListener("click", highScoreHref);
+
+
 
 
